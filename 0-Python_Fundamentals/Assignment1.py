3afef89c7f8a1faa3c41b2bdad7c28d91de7d86d
suman panda <pandasuman12345@gmail.com>
2026-01-30 00:11:45 +0530

    python fundamentals

--- 0-Python_Fundamentals/Assignment1.py
+++ 0-Python_Fundamentals/Assignment1.py
@@ -1,11 +1,4 @@
-# Write a python program to print your name 10 times
-
-# name = "Suman Panda"
-# for i in range(1,11):
-#     print(f"{i} {name}")
-
 
 # Assigment Question : print your name 10 times using * symbol and each time it should be on next line
 
-# print("Suman panda \n" * 10)
 print(("Suman panda" + "\n") * 10)
---
+++ 0-Python_Fundamentals/assignment.py
@@ -0,0 +1,9 @@
+# Write a python program to print your name 10 times
+
+# name = "Suman Panda"
+# for i in range(1,11):
+#     print(f"{i} {name}")
+
+# or
+
+print("Suman panda \n" * 10)
