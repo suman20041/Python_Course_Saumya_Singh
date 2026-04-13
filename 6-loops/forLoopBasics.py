54ab96d0c79acdc70e035693c9afd74c3e08457d
suman panda <pandasuman12345@gmail.com>
2026-04-13 19:27:44 +0530

    loops

--- 6-loops/forLoopBasics.py
+++ 6-loops/forLoopBasics.py
@@ -5,7 +5,11 @@ foodList = ["cake", "mango", "pizza"]
 for item in foodList:
     print(item)
 
+# item iterator and ["cake", "mango", "pizza"] iteration means jaise list hai tho uska har ake item ko visit karna
+
 collegesTuple = ("NIT-Delhi", "ABES", "BBAU", "IIT-D", "IIT-H")
 
 for eachItem in collegesTuple:
     print("Colleges Visited by Saumya",eachItem)
+
+# list aur tuple mai kaise trversal or iterate karte hai 
